15a84256388badffc5e51b8b557a7eda69379482
Diego Ciros Ortíz~ <diegociros@ciencias.unam.mx>
2026-09-13 13:58:31 -0600

    Agregar colores a las estrategias de Dittuu

--- Practica01/EstrategiaDittuuVidaTormentosa.java
+++ Practica01/EstrategiaDittuuVidaTormentosa.java
@@ -5,13 +5,16 @@ import java.util.Random;
  * y genera una tormenta de salud exclusiva al defenderse con calculos de aura.
  */
 public class EstrategiaDittuuVidaTormentosa implements EstrategiaPelea {
+    public static final String ROJO = "\u001B[31m";
+    public static final String VERDE = "\u001B[32m";
+    public static final String RESET = "\u001B[0m";
     private Random random = new Random();
 
     @Override
     public int atacar(Combatiente objetivo) {
         int auraObtenida = 1000 + random.nextInt(2001);
         System.out.println("¡Dittuu desata una tormenta de energia pura sobre " + objetivo.getNombre() + ", arrasando con todo!");
-        System.out.println("+ " + auraObtenida + " de Aura 🗿🔥");
+        System.out.println(VERDE + "+ " + auraObtenida + " de Aura 🗿🔥" + RESET);
         return auraObtenida; 
     }
 
@@ -19,7 +22,7 @@ public class EstrategiaDittuuVidaTormentosa implements EstrategiaPelea {
     public int defender(Combatiente atacante) {
         int auraPerdida = 1000 + random.nextInt(2001);
         System.out.println("¡Dittuu gira velozmente ante " + atacante.getNombre() + ", generando una tormenta de salud exclusiva para el!");
-        System.out.println("- " + auraPerdida + " de Aura 🥶💦");
+        System.out.println(ROJO + "- " + auraPerdida + " de Aura 🥶💦" + RESET);
         return auraPerdida; 
     }
 
